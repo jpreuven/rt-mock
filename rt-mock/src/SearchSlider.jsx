5e765746c3f8344bc8f6f9733d2db16b2a0d548b
Jonathan <jpreuven@gmail.com>
2023-11-01 06:08:25 -0700

    added images + descriptions to banners

--- rt-mock/src/SearchSlider.jsx
+++ rt-mock/src/SearchSlider.jsx
@@ -2,115 +2,68 @@ import { Flex } from "@chakra-ui/react";
 import React, { Component } from "react";
 import Slider from "react-slick";
 
-export default class searchSlider extends Component {
-  render() {
+export default function searchSlider() {
+  {
+    const imageFolder = `${import.meta.env.BASE_URL}images/banner/`;
+
+    const bannerImages = [
+      {
+        name: "Cat sunglasses.jpg",
+        description:
+          " Lorem ipsum dolor sit, amet consectetur adipisicing elit.Ipsum tempora, corrupti reprehenderit similique quidem vitae at, cum dicta ducimus unde ipsam sequi nostrum repellendus sed suscipit maiores id optio ratione laborum error expedita consequatur. Aspernatur eos tempora dolores asperiores fugiat, autem quibusdam delectus. Praesentium tempora fuga qui accusantium sed, fugiat sequi nemo esse eligendi sunt corporis ",
+        //Ipsum tempora, corrupti reprehenderit similique quidem vitae at, cum dicta ducimus unde ipsam sequi nostrum repellendus sed suscipit maiores id optio ratione laborum error expedita consequatur. Aspernatur eos tempora dolores asperiores fugiat, autem quibusdam delectus. Praesentium tempora fuga qui accusantium sed, fugiat sequi nemo esse eligendi sunt corporis omnis dolores nesciunt et cumque quas laborum eaque commodi ab enim quis consectetur assumenda ullam pariatur. Optio rerum quas ipsa molestiae ea qui facilis, assumenda natus officia impedit quod consectetur pariatur. Facilis obcaecati doloribus tempore amet asperiores voluptatem voluptas? Quae eius ratione quisquam eum!
+      },
+      {
+        name: "Movie set.jpg",
+        description: "Vivamus lacinia odio vitae vestibulum.",
+      },
+      {
+        name: "Screenshot 2023-11-02 112659.jpg",
+        description: "Donec sit amet nisl. Sed massa.",
+      },
+      {
+        name: "camera-director.jpg",
+        description: "Curabitur sodales ligula in libero.",
+      },
+      {
+        name: "explosion movie set.jpg",
+        description: "Sed dignissim lacinia nunc.",
+      },
+    ];
+
     var settings = {
       infinite: true,
-      slidesToShow: 3,
+      slidesToShow: 2,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 4000,
+      autoplaySpeed: 999999999,
       pauseOnHover: true,
     };
     return (
       <div>
         <Slider {...settings}>
-          <div>
-            <h3
-              style={{
-                background: "#5f9ea0",
-                color: "#fff",
-                fontSize: "36px",
-                lineHeight: "100px",
-                margin: "10px",
-                padding: "2%",
-                position: "relative",
-                textAlign: "center",
-              }}
-            >
-              1
-            </h3>
-          </div>
-          <div>
-            <h3
-              style={{
-                background: "#5f9ea0",
-                color: "#fff",
-                fontSize: "36px",
-                lineHeight: "100px",
-                margin: "10px",
-                padding: "2%",
-                position: "relative",
-                textAlign: "center",
-              }}
-            >
-              2
-            </h3>
-          </div>
-          <div>
-            <h3
-              style={{
-                background: "#5f9ea0",
-                color: "#fff",
-                fontSize: "36px",
-                lineHeight: "100px",
-                margin: "10px",
-                padding: "2%",
-                position: "relative",
-                textAlign: "center",
-              }}
-            >
-              3
-            </h3>
-          </div>
-          <div>
-            <h3
-              style={{
-                background: "#5f9ea0",
-                color: "#fff",
-                fontSize: "36px",
-                lineHeight: "100px",
-                margin: "10px",
-                padding: "2%",
-                position: "relative",
-                textAlign: "center",
-              }}
-            >
-              4
-            </h3>
-          </div>
-          <div>
-            <h3
-              style={{
-                background: "#5f9ea0",
-                color: "#fff",
-                fontSize: "36px",
-                lineHeight: "100px",
-                margin: "10px",
-                padding: "2%",
-                position: "relative",
-                textAlign: "center",
-              }}
-            >
-              5
-            </h3>
-          </div>
-          <div>
-            <h3
-              style={{
-                background: "#5f9ea0",
-                color: "#fff",
-                fontSize: "36px",
-                lineHeight: "100px",
-                margin: "10px",
-                padding: "2%",
-                position: "relative",
-                textAlign: "center",
-              }}
-            >
-              6
-            </h3>
-          </div>
+          {bannerImages.map((image, index) => (
+            <div key={index} id="banner-image-container">
+              <img
+                src={`${imageFolder}${encodeURIComponent(image.name)}`}
+                alt={`Banner ${index}`}
+                style={{ width: "100%" }}
+              />
+              <div
+                id="banner-image-description"
+                style={{
+                  position: "absolute",
+                  bottom: 10,
+                  background: "rgba(0, 0, 0, 0.6)",
+                  color: "white",
+                  width: "100%",
+                  wordWrap: "break-word",
+                }}
+              >
+                <p>{image.description}</p>
+              </div>
+            </div>
+          ))}
         </Slider>
       </div>
     );
--- rt-mock/src/TVpage.jsx
+++ rt-mock/src/TVpage.jsx
@@ -17,9 +17,27 @@ export default function TVpage() {
       .then((r) => r.json())
       .then((data) => setPageData(data));
   }, []);
+  console.log(pageData);
 
   if (!pageData) {
     return <h1>Data not yet returned</h1>;
   }
-  return <h1>Data for {id} returned</h1>;
+  return (
+    <div style={{ position: "relative", display: "inline-block" }}>
+      <img src={`https://image.tmdb.org/t/p/w500${pageData.poster_path}`} />
+      <div
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          background: "rgba(0, 0, 0, 0.6)", // This gives the transparent black background
+          color: "white",
+          padding: "10px",
+        }}
+      >
+        <p>hello?</p>
+      </div>
+    </div>
+  );
 }
